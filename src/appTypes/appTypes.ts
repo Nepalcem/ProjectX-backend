bbff9882f8fc9e4233fea17b4adbb168ad8d7f24
MarkW <mark.emailtest@gmail.com>
2026-01-27 23:58:36 +0200

    character interface

--- src/appTypes/appTypes.ts
+++ src/appTypes/appTypes.ts
@@ -2,6 +2,28 @@ export interface IUser {
   email: string;
   password: string;
   nickname: string;
+  token: string | null;
   verified: boolean;
   verificationToken: string;
 }
+
+export interface ICharacter {
+  id: string;
+  race: IRace;
+  level: number;
+  owner: string; // user ID
+  createdAt: Date;
+  strength: number;
+  agility: number;
+  luck: number;
+  vitality: number;
+  health: number;
+  healthRecovery: number;
+  experience: number;
+  fatigue: number;
+  fatigueRecovery: number;
+  statPoints: number;
+  money: number;
+}
+
+export type IRace = "human" | "elf" | "dwarf" | "orc";
--- src/models/user/user.ts
+++ src/models/user/user.ts
@@ -7,34 +7,39 @@ import {
 
 import type { IUser } from "@/appTypes/appTypes.js";
 
-const userSchema: Schema = new Schema<IUser>({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: emailRegExp,
+const userSchema: Schema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: emailRegExp,
+    },
+    password: {
+      type: String,
+      required: true,
+      match: passwordRegExp,
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    nickname: {
+      type: String,
+      required: true,
+      unique: true,
+      match: nicknameRegExp,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
-  password: {
-    type: String,
-    required: true,
-    match: passwordRegExp,
-  },
-  nickname: {
-    type: String,
-    required: true,
-    unique: true,
-    match: nicknameRegExp,
-  },
-  verified: {
-    type: Boolean,
-    default: false,
-  },
-  verificationToken: {
-    type: String,
-    required: [true, "Verify token is required"],
-  },
-});
-
-
+  { versionKey: false, timestamps: true },
+);
 
 export default model<IUser>("User", userSchema);

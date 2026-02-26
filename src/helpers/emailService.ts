a0c46586e05fd0fb7a2b179fb057d3b361b952fb
MarkW <mark.emailtest@gmail.com>
2026-02-27 01:11:33 +0200

    setup brevo for mailing, waiting the response from support

--- src/helpers/emailService.ts
+++ src/helpers/emailService.ts
@@ -1,4 +1,12 @@
-import axios from "axios";
+import { BrevoClient } from "@getbrevo/brevo";
+
+const { BREVO_API_KEY, BREVO_FROM_EMAIL, BREVO_FROM_NAME } = process.env;
+
+if (!BREVO_API_KEY || !BREVO_FROM_EMAIL || !BREVO_FROM_NAME) {
+  throw new Error("Brevo environment variables are missing");
+}
+
+const brevo = new BrevoClient({ apiKey: BREVO_API_KEY });
 
 interface SendEmailOptions {
   to: string;
@@ -6,38 +14,25 @@ interface SendEmailOptions {
   html: string;
 }
 
-const { ELASTIC_API_KEY, ELASTIC_FROM_EMAIL } = process.env;
-
-  if (!ELASTIC_API_KEY || !ELASTIC_FROM_EMAIL) {
-    throw new Error("Elastic Email environment variables are missing");
-  }
-
 const sendVerificationEmail = async ({
   to,
   subject,
   html,
 }: SendEmailOptions): Promise<void> => {
-  await axios.post(
-    "https://api.elasticemail.com/v4/emails",
-    {
-      Recipients: [{ Email: to }],
-      Content: {
-        From: ELASTIC_FROM_EMAIL,
-        Subject: subject,
-        Body: [
-          {
-            ContentType: "HTML",
-            Content: html,
-          },
-        ],
+  try {
+    await brevo.transactionalEmails.sendTransacEmail({
+      sender: {
+        email: BREVO_FROM_EMAIL,
+        name: BREVO_FROM_NAME,
       },
-    },
-    {
-      headers: {
-        "X-ElasticEmail-ApiKey": ELASTIC_API_KEY,
-      },
-    },
-  );
+      to: [{ email: to }],
+      subject,
+      htmlContent: html,
+    });
+  } catch (error: any) {
+    console.error(error.response?.body || error);
+    throw error;
+  }
 };
 
 export default sendVerificationEmail;
--- src/zodSchemas/registerSchema.ts
+++ src/zodSchemas/registerSchema.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
 import {
   passwordRegExp,
-  emailRegExp,
-  nicknameRegExp,
+  emailRegExp
 } from "@/constants/regularExpressions.js";
 
 const registerSchema = z.object({
@@ -15,13 +14,6 @@ const registerSchema = z.object({
     .regex(
       passwordRegExp,
       "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, and one number or special character",
-    ),
-  nickname: z
-    .string({ error: "Nickname must be a string" })
-    .max(16, { error: "Nickname must be no more than 16 symbols" })
-    .regex(
-      nicknameRegExp,
-      "Nickname must be 3 to 24 characters long and can only contain letters, numbers, and underscores",
     ),
   token: z
     .string({ error: "Token must be a string" })

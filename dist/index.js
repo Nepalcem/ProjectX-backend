0d46da2fbe344e5e8a6e0630853c2d6652dd1f7c
MarkW <mark.emailtest@gmail.com>
2026-03-10 22:31:44 +0200

    render limiter configuration fix & index.ts & app.ts logic  split

--- dist/index.js
+++ dist/index.js
@@ -1,34 +1,16 @@
-import express from "express";
 import mongoose from "mongoose";
-import logger from "morgan";
-import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import AppError from "./errorHandler/errorHandler.js";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: path.join(__dirname, "../environment", ".env") });
-const { PORT } = process.env || 3000;
+const { app } = await import("./app.js");
+const PORT = process.env.PORT || 3000;
 const { MONGO_URL } = process.env;
 if (!MONGO_URL) {
     console.error("MONGO_URL is not defined in the environment variables.");
     process.exit(1);
 }
-const authRouter = await import("./routes-Api/authRouter.js");
-const app = express();
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-app.use(logger(formatsLogger));
-app.use(cors());
-app.use(express.json());
-app.use((err, _req, res, _next) => {
-    const { status = 500, message = "Server error" } = err;
-    res.status(status).json({ message: message });
-    return Promise.resolve();
-});
-app.get("/", (_req, res) => {
-    res.send(console.log("Its alive!1"));
-});
-app.use("/auth", authRouter.default);
 mongoose
     .connect(MONGO_URL)
     .then(() => {
---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import type { Express, NextFunction, Request, Response } from "express";
+import AppError from "./errorHandler/errorHandler.js";
+
+const authRouter = await import("./routes-Api/authRouter.js");
+
+export const app: Express = express();
+
+app.set("trust proxy", 1);
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send(console.log("Its alive!1"));
+});
+
+app.use("/auth", authRouter.default);
+app.use((err: AppError, _req: Request, res: Response, _next: NextFunction) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
